981af1cf91174b96cf19819a12bdaa9c7366c0e8
Mohammad Ezmerli <mohammad.ezmerli@gmx.de>
2021-04-13 21:13:31 +0200

    added validation for mongoUser

--- ScrumRandomizerServer.Data/Users/Models/MongoUser.cs
+++ ScrumRandomizerServer.Data/Users/Models/MongoUser.cs
@@ -2,6 +2,7 @@
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.IdGenerators;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ScrumRandomizerServer.Data.Users.Models
 {
@@ -12,6 +13,8 @@ namespace ScrumRandomizerServer.Data.Users.Models
         [BsonRepresentation(BsonType.String)]
         public Guid Id { get; set; } = Guid.Empty;
 
+        [Required]
+        [StringLength(maximumLength: 32, MinimumLength = 1)]
         public string Name { get; set; } = string.Empty;
     }
 }
